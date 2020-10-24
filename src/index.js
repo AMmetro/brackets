84a768481344a62060993ea4ffe5626a147f96e1
Andrei Mitskevich <metro.trend.now@gmail.com>
2020-10-24 14:25:04 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+   module.exports = function check(str, bracketsConfig) {
+  
+     let bracketsConfigObj = new Map(bracketsConfig);
+
+       let arr = [];
+
+    for (let ind of str) {
+      
+        if ( ind ==  bracketsConfigObj.get(arr[arr.length - 1]) )
+      
+              arr.pop()
+
+        else {arr.push(ind)}
+
+      }
+
+       return !arr.length
+  }
